d049b7e48baf72984c23ff6780c4dde80fffd174
Jai Ganesh Suresh <30567106+Jaish19@users.noreply.github.com>
2025-12-26 01:45:46 +0530

    Refactored the imports of langchain and their dependencies

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import streamlit as st
 from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.question_answering import load_qa_chain
-from langchain.prompts import PromptTemplate
+from langchain_classic.chains.question_answering import load_qa_chain
+from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 
 
 import os
@@ -122,4 +122,5 @@ def main():
 
 
 if __name__ == "__main__":
+
     main()
